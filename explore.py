59c2c8c52089cfbebfa76048a36cdefd778cba22
Oliver Ton <oliver.ton75@gmail.com>
2022-05-14 00:12:53 -0500

    add word cloud function in case want to use in final notebook

--- explore.py
+++ explore.py
@@ -4,6 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import json
 import matplotlib as mpl
+import nltk
+from wordcloud import WordCloud
 
 
 def word_counts(train):
@@ -32,13 +34,59 @@ def word_counts(train):
 
 
 def common_words(word_counts):
-    for column in word_counts.drop(columns='all'):
+    '''
+    This function take in a variable from word_count function and create a stack bar plot 
+    to see which word is common in the README for each category of langue
+    '''
     # axis=1 in .apply means row by row
-        (word_counts.sort_values(by='all', ascending=False)
-        .head(20).apply(lambda row: row / row['all'], axis=1).drop(columns='all')
-        .sort_values(by= column).plot.barh(stacked=True, width=1, ec='black'))
-        plt.title(f'% of  the most common 20 {column} README words')
-        plt.legend(bbox_to_anchor= (1.03,1))
+    word_counts.sort_values(by='all', ascending=False).head(30)\
+    .apply(lambda row: row / row['all'], axis=1).sort_values(by='all')\
+    .drop(columns='all')\
+    .plot.barh(stacked=True, width=1, ec='black')
 
-        plt.gca().xaxis.set_major_formatter(mpl.ticker.FuncFormatter('{:.0%}'.format))
-        plt.show()
+    plt.title(f'% of the most common 30 words')
+    plt.legend(bbox_to_anchor= (1.03,1))
+
+    plt.gca().xaxis.set_major_formatter(mpl.ticker.FuncFormatter('{:.0%}'.format))
+    plt.show()
+
+
+def word_cloud (text):
+    '''
+    takes in a text and create a wordcloud
+    '''
+    img = WordCloud(background_color='white', width=800, height=600).generate(text)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+
+def wordcloud_top(df,column, n_top=50):
+    '''
+    takes in a df , column and a number of top words to show
+    '''
+    top_all =df.sort_values(column, ascending=False)[[column]].head(n_top)
+    word_cloud(' '.join(top_all.index))
+
+
+def ngrams_wordcloud (text, title,  n=2, top = 20):
+    '''
+    takes in a text, title, number of ngrams, and number of the top words
+    returns a plot barh and a word_cloud
+    '''
+    #plot barh
+    
+    plt.subplot(2,2,1)
+    pd.Series(nltk.ngrams(text.split(), n=n)).value_counts().head(top).sort_values(ascending = True).plot.barh()
+    plt.title(f'Top {top} most common {title} ngrams where n={n}')
+    
+    #word_cloud
+    ng =(pd.Series(nltk.ngrams(text.split(), n=n)).value_counts().head(top)).to_dict()
+    ng_words = {k[0] + ' ' + k[1]: v for k, v in ng.items()}
+    plt.subplot(2,2,2)
+    img = WordCloud(background_color='white', width=800, height=600).generate_from_frequencies(ng_words)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.title(f'Top {top} most common {title} ngrams where n={n}')
+    #plt.tight_layout()
+    plt.show()
